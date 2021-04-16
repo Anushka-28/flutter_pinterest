55b8f04ae68c0643c51a7901f013764942dd5a46
Anushka Chauhan <anushkachauhan1503@gmail.com>
2021-04-17 00:03:34 +0530

    Implemented GridView widget. (Initial copied code)

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,53 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
+//Copied from -> https://medium.com/globant/gridview-in-flutter-ede1df032fe7
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'GridView Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blueGrey[900],
-          title: Center(
-            child: Text(
-              'Flutter GridView',
-              style: TextStyle(
-                color: Colors.blueAccent,
-                fontWeight: FontWeight.bold,
-                fontSize: 20.0,
-              ),
-            ),
-          ),
-        ),
-        body: Column(children: <Widget>[
-          Row(
-            //ROW 1
-            children: [
-              getContainer(),
-              getContainer(),
-              getContainer(),
-              getContainer(),
-            ],
-          ),
-          Row(//ROW 2
-              children: [
-            getContainer(),
-            getContainer(),
-            getContainer(),
-            getContainer(),
-          ]),
-          Row(
-            //ROW 3
-            children: [
-              getContainer(),
-              getContainer(),
-              getContainer(),
-              getContainer(),
-            ],
-          ),
-          Row(//ROW 4
-              children: [
-            getContainer(),
-            getContainer(),
-            getContainer(),
-            getContainer(),
-          ]),
-          Row(//ROW 5
-              children: [
-            getContainer(),
-            getContainer(),
-            getContainer(),
-            getContainer(),
-          ]),
-        ]),
-      ),
+      home: GridList(),
     );
   }
+}
 
-  Widget getContainer() {
-    return Container(
-      height: 200,
-      width: 200,
-      //color: Colors.blue,
-      margin: EdgeInsets.all(25.0),
-      color: Colors.blue,
+class GridList extends StatelessWidget {
+  final List<String> _list = [
+    "Text 01",
+    "Text 02",
+    "Text 03",
+    "Text 04",
+    "Text 05",
+    "Text 06",
+  ];
+//list of strings to display in grid
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GridView Demo'),
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: EdgeInsets.all(5.0),
+        crossAxisSpacing: 5.0,
+        mainAxisSpacing: 5.0,
+        children: _list
+            .map((data) => Card(
+                  child: Center(
+                      child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(data),
+                  )),
+                ))
+            .toList(),
+      ),
     );
   }
 }
